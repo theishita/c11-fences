c25eef9499ec6dba33a7849c57860dd3c3d2f8b3
theishita <ishijaju@gmail.com>
2020-01-02 17:45:51 +0530

    Added functionality for collecting sb, sw

--- processing.py
+++ processing.py
@@ -1,56 +1,109 @@
-
+# --------------------------------------------------------
+# Processes the std:out data from the command line output
+# and creates lists of Synchronized-Waits (sw) and 
+# Sequenced-Befores (sb).
+# --------------------------------------------------------
 
 class Processing:
     def __init__(self,p):
 
-        self.traces = []                                        # contains lists of all execution traces
+        self.traces = []                                            # lists of all execution traces
+        self.events_order = []                                      # order of events including fences
+        self.sw_edges = []                                          # list of sw edges
+        self.sb_edges = []                                          # list of sb edges
 
-        f=0                                                     # flag for finding execution trace
+        f=0                                                         # flag for finding execution trace
         for line in p.split('\n'):
 
 
             if f==2:
-                if "HASH" in line:                              # indicates end of one execution trace
+                if "HASH" in line:                                  # indicates end of one execution trace
                     f=0
-                    self.traces.append(trace)
+                    self.traces.append(trace_list)
                 else:
-                    trace.append(line.split())                  # collect data from the execution trace and convert to list structure
+                    trace_list.append(line.split())                 # collect data from the execution trace and convert to list structure
  
             if f==1:
                 f=2
 
             # check assertion violation case
             if 'Rf' in line:
-                trace = []
+                trace_list = []
                 f=1
 
-        self.fence()
+        for trace in self.traces:
+            self.fence(trace)
+
+        # print(self.events_order)
+        # print(self.sw_edges)
+        print(self.sb_edges)
         
-    def fence(self):
+    def fence(self,trace):
 
         # find out the number of threads in the program
         threads = 0
-        for a in self.traces:
-            for b in a:
-                threads = max(threads,int(b[1]))
+        for a in trace:
+            threads = max(threads,int(a[1]))
         
         print("Number of threads (including main thread) =",threads)
 
-        self.events_order = []                                  # order of events including fences
 
-        for a in self.traces:
-            fences = 1
-            print("trace n")
+        fences = 1
+        print("trace n")
 
-            exec = ['F1']
+        exec = ['F1']
 
-            for j in range(2,threads+1):
-                for i in range(len(a)):
-                    if int(a[i][1])==j:
-                        exec.append('E'+a[i][0])
-                        fences+=1
-                        exec.append('F'+str(fences))
-            
-            self.events_order.append(exec)
+        for j in range(2,threads+1):
+            for i in range(len(trace)):
+                if int(trace[i][1])==j:
+                    event = {'no': trace[i][0],                         # trace[i][0] is the event number
+                            'thread': j
+                    }
+                    if trace[i][3]=='read':
+                        event["type"] = "read"
+                        event['rf'] = trace[i][7]                       # trace[i][7] gives Read-from (Rf)
+                    elif trace[i][3]=='write':
+                        event["type"] = "write"
+                    exec.append(event)
+                    fences+=1
+                    exec.append('F'+str(fences))
+        
+        self.events_order.append(exec)
+        self.sw(exec)
+        self.sb(exec,threads)
 
-        print(self.events_order)
+    def sw(self,trace):
+
+        for i in range(len(trace)):
+
+            if 'rf' in trace[i]:
+                sw = []                                                  # list of sw edges in this execution
+                flag = 0                                                 # flag to add only the required edges
+                rf = trace[i]['rf']
+                f1 = trace[i+1]
+
+                for j in range(len(trace)):
+                    if 'no' in trace[j] and trace[j]['no']==rf:
+                        flag = 1
+                        f2 = trace[j-1]
+                        sw.append((f2,f1))
+
+                if flag == 1:
+                    self.sw_edges.append(sw)
+
+    def sb(self, trace, threads):
+
+        edges = []                                                          # list to store sb's of an execution
+
+        for i in range(2,threads+1):
+            sb = []                                                         # list to store sb's of each thread
+            for j in range(len(trace)):
+                if 'thread' in trace[j] and trace[j]['thread']==i:
+                    if trace[j-1] not in sb:
+                        sb.append(trace[j-1])
+                    if i==threads:
+                        sb.append(trace[j+1])
+  
+            edges.append(tuple(sb))
+
+        self.sb_edges.append(edges)
