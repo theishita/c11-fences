4108e7c5d695e6c67ae57ed1a8778283947d6684
theishita <ishijaju@gmail.com>
2020-01-02 14:05:13 +0530

    Added event fences

--- processing.py
+++ processing.py
@@ -23,10 +23,34 @@ class Processing:
             if 'Rf' in line:
                 trace = []
                 f=1
-        
-        for a in self.traces:
-            print("trace n")
-            for b in a:
-                print(b)
 
+        self.fence()
         
+    def fence(self):
+
+        # find out the number of threads in the program
+        threads = 0
+        for a in self.traces:
+            for b in a:
+                threads = max(threads,int(b[1]))
+        
+        print("Number of threads (including main thread) =",threads)
+
+        self.events_order = []                                  # order of events including fences
+
+        for a in self.traces:
+            fences = 1
+            print("trace n")
+
+            exec = ['F1']
+
+            for j in range(2,threads+1):
+                for i in range(len(a)):
+                    if int(a[i][1])==j:
+                        exec.append('E'+a[i][0])
+                        fences+=1
+                        exec.append('F'+str(fences))
+            
+            self.events_order.append(exec)
+
+        print(self.events_order)
