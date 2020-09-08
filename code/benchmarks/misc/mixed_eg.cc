254433b5fbd7eb921478f573daa258e97296cef3
Ishita Jaju <ishijaju@gmail.com>
2020-09-08 15:04:08 +0530

    fixed bm

--- code/benchmarks/misc/mixed_eg.cc
+++ code/benchmarks/misc/mixed_eg.cc
@@ -11,20 +11,19 @@ atomic<int> z;
 atomic<int> a;
 atomic<int> b;
 atomic<int> c;
-atomic<int> dum_1;
 
 static void t1(void *arg) {
 	z.store(1, memory_order_relaxed);
 	x.store(1, memory_order_relaxed);
     int temp = y.load(memory_order_relaxed);
-	a.store(temp,memory_order_relaxed);
+	a.store(temp, memory_order_relaxed);
 }
 
 static void t2(void *arg) {
 	y.store(1, memory_order_relaxed);
-    int temp = x.load(memory_order_relaxed);
+	int temp = x.load(memory_order_relaxed);
 	b.store(temp, memory_order_relaxed);
-    int temp2 = z.load(memory_order_relaxed);
+	int temp2 = z.load(memory_order_relaxed);
 	c.store(temp2, memory_order_relaxed);
 }
 
@@ -37,7 +36,6 @@ int user_main(int argc, char **argv) {
 	atomic_init(&a, 0);
 	atomic_init(&b, 0);
     atomic_init(&c, 0);
-    atomic_init(&dum_1, 0);
 
 	thrd_create(&id1, (thrd_start_t)&t1, NULL);
     thrd_create(&id2, (thrd_start_t)&t2, NULL);
@@ -46,6 +44,7 @@ int user_main(int argc, char **argv) {
     thrd_join(id2);
 
     MODEL_ASSERT ((a.load(memory_order_relaxed) != 0 || b.load(memory_order_relaxed) != 0) && ((b.load(memory_order_relaxed) != 1) || (c.load(memory_order_relaxed) == 1)));
+	// abc cannot be 000, 001, 011, 110
 
 	return 0;
 }
