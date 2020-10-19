fb6444c31b4adf80deaae7347c13c0ae9ae16c21
Ishita Jaju <ishijaju@gmail.com>
2020-10-19 10:53:34 +0530

    Added header comments in misc benchmarks

--- code/benchmarks/misc/basic.cc
+++ code/benchmarks/misc/basic.cc
@@ -1,7 +1,11 @@
-#include <iostream>
+/**
+ * misc - basic
+*/
+
+#include <model-assert.h>
 #include <threads.h>
 #include <atomic>
-#include <model-assert.h>
+#include <iostream>
 
 using namespace std;
 
@@ -12,18 +16,18 @@ atomic<int> b;
 
 static void t1(void *arg) {
 	x.store(__LINE__, 1, memory_order_relaxed);
-    int temp = y.load(__LINE__, memory_order_relaxed);
-	if(temp == 1)
-    	a.store(__LINE__, 1, memory_order_relaxed);
+	int temp = y.load(__LINE__, memory_order_relaxed);
+	if (temp == 1)
+		a.store(__LINE__, 1, memory_order_relaxed);
 	else
 		a.store(__LINE__, 0, memory_order_relaxed);
 }
 
 static void t2(void *arg) {
 	y.store(__LINE__, 1, memory_order_relaxed);
-    int temp = x.load(__LINE__, memory_order_relaxed);
-	if(temp == 1)
-    	b.store(__LINE__, 1, memory_order_relaxed);
+	int temp = x.load(__LINE__, memory_order_relaxed);
+	if (temp == 1)
+		b.store(__LINE__, 1, memory_order_relaxed);
 	else
 		b.store(__LINE__, 0, memory_order_relaxed);
 }
@@ -31,19 +35,19 @@ static void t2(void *arg) {
 int user_main(int argc, char **argv) {
 	thrd_t id1, id2;
 
-    atomic_init(&x, 0);
+	atomic_init(&x, 0);
 	atomic_init(&y, 0);
 	atomic_init(&a, 0);
-    atomic_init(&b, 0);
+	atomic_init(&b, 0);
 
 	thrd_create(&id1, (thrd_start_t)&t1, NULL);
-    thrd_create(&id2, (thrd_start_t)&t2, NULL);
+	thrd_create(&id2, (thrd_start_t)&t2, NULL);
 
-    thrd_join(id1);
-    thrd_join(id2);
+	thrd_join(id1);
+	thrd_join(id2);
 
-    MODEL_ASSERT (a.load(__LINE__, memory_order_relaxed) != 0 ||
-					b.load(__LINE__, memory_order_relaxed) != 0);
+	MODEL_ASSERT(a.load(__LINE__, memory_order_relaxed) != 0 ||
+				 b.load(__LINE__, memory_order_relaxed) != 0);
 
 	return 0;
 }
--- code/benchmarks/misc/mixed_eg.cc
+++ code/benchmarks/misc/mixed_eg.cc
@@ -1,7 +1,11 @@
-#include <iostream>
+/**
+ * misc - mixed_eg
+*/
+
+#include <model-assert.h>
 #include <threads.h>
 #include <atomic>
-#include <model-assert.h>
+#include <iostream>
 
 using namespace std;
 
@@ -15,7 +19,7 @@ atomic<int> c;
 static void t1(void *arg) {
 	z.store(__LINE__, 1, memory_order_relaxed);
 	x.store(__LINE__, 1, memory_order_relaxed);
-    int temp = y.load(__LINE__, memory_order_relaxed);
+	int temp = y.load(__LINE__, memory_order_relaxed);
 	a.store(__LINE__, temp, memory_order_relaxed);
 }
 
@@ -30,20 +34,20 @@ static void t2(void *arg) {
 int user_main(int argc, char **argv) {
 	thrd_t id1, id2;
 
-    atomic_init(&x, 0);
-    atomic_init(&y, 0);
+	atomic_init(&x, 0);
+	atomic_init(&y, 0);
 	atomic_init(&z, 0);
 	atomic_init(&a, 0);
 	atomic_init(&b, 0);
-    atomic_init(&c, 0);
+	atomic_init(&c, 0);
 
 	thrd_create(&id1, (thrd_start_t)&t1, NULL);
-    thrd_create(&id2, (thrd_start_t)&t2, NULL);
+	thrd_create(&id2, (thrd_start_t)&t2, NULL);
 
-    thrd_join(id1);
-    thrd_join(id2);
+	thrd_join(id1);
+	thrd_join(id2);
 
-    MODEL_ASSERT ((a.load(__LINE__, memory_order_relaxed) != 0 || b.load(__LINE__, memory_order_relaxed) != 0) && ((b.load(__LINE__, memory_order_relaxed) != 1) || (c.load(__LINE__, memory_order_relaxed) == 1)));
+	MODEL_ASSERT((a.load(__LINE__, memory_order_relaxed) != 0 || b.load(__LINE__, memory_order_relaxed) != 0) && ((b.load(__LINE__, memory_order_relaxed) != 1) || (c.load(__LINE__, memory_order_relaxed) == 1)));
 	// abc cannot be 000, 001, 011, 110
 
 	return 0;
